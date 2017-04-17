f931f801987718e20d1f6c33b3208f782b281c88
masuyama <masuyama@synchro-food.co.jp>
2017-04-17 14:45:51 +0900

    .gitignoreにファイルを追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e0eb974acbd1adde024a73245291aba09394c79eedbb5787a9da5a37b03841f5b4a430f73f5890b4872c22f90aea7cc5b5b69786a0bb24154902764de1b20ae9'
